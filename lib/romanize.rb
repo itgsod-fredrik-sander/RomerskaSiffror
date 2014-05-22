6b0b1b34fde555cbf0ae70835e317142e9213df7
Fredrik Sander <fredde.sander@gmail.com>
2014-05-22 09:19:34 +0200

    Added member method 'to_a'  Integer class

--- lib/romanize.rb
+++ lib/romanize.rb
@@ -6,6 +6,10 @@ class Integer
 
     return false
   end
+
+  def to_a
+    return self.to_s.scan(/./).map {|value| value.to_i }
+  end
 end
 
 def romanize(number)
@@ -13,6 +17,7 @@ def romanize(number)
   raise ArgumentError, 'can not encode negative number' if number.negative?
   
   arabic_roman_numbers = {1 => "I", 5 => "V", 10 => "X", 50 => "L", 100 => "C", 500 => "D", 1000 => "M"}
+  number = number.to_a
 
   return "string"
 end
--- spec/romanize_spec.rb
+++ spec/romanize_spec.rb
@@ -3,24 +3,24 @@ require_relative '../lib/romanize'
 
 describe 'converting arabic numerals to roman' do
 
-  it 'should take a number as argument' do
-    expect { romanize() }.to raise_error ArgumentError
-    expect { romanize(1000) }.not_to raise_error
-  end
+  #it 'should take a number as argument' do
+  #  expect { romanize() }.to raise_error ArgumentError
+  #  expect { romanize(1000) }.not_to raise_error
+  #end
 
   #Detta test kan kommenteras bort om man inte vill test 'Undantagshantering' på C- eller A-nivå
-  it 'should raise ArgumentError with correct error message if called with zero (romans had no way of writing zero' do
-    expect { romanize(0) }.to raise_error ArgumentError, 'can not encode zero'
-  end
+  #it 'should raise ArgumentError with correct error message if called with zero (romans had no way of writing zero' do
+  #  expect { romanize(0) }.to raise_error ArgumentError, 'can not encode zero'
+  #end
 
   #Detta test kan kommenteras bort om man inte vill test 'Undantagshantering' på C- eller A-nivå
-  it 'should raise ArgumentError with correct error message if called with negative number' do
-    expect { romanize(rand(1000) * -1) }.to raise_error ArgumentError, 'can not encode negative number'
-  end
+  #it 'should raise ArgumentError with correct error message if called with negative number' do
+  #  expect { romanize(rand(1000) * -1) }.to raise_error ArgumentError, 'can not encode negative number'
+  #end
 
-  it 'should return a string' do
-    expect(romanize(rand(1000))).to be_instance_of String
-  end
+  #it 'should return a string' do
+  #  expect(romanize(rand(1000))).to be_instance_of String
+  #end
 
   #it 'should encode single-digit numbers' do
   #  romanize(1).should match 'I'
@@ -34,15 +34,15 @@ describe 'converting arabic numerals to roman' do
   #  romanize(9).should match 'IX'
   #end
 
-  #it 'should encode double digit numbers' do
+  it 'should encode double digit numbers' do
   #  romanize(10).should match 'X'
   #  romanize(12).should match 'XII'
   #  romanize(20).should match 'XX'
   #  romanize(36).should match 'XXXVI'
-  #  romanize(44).should match 'XLIV'
+     romanize(44).should match 'XLIV'
   #  romanize(87).should match 'LXXXVII'
   #  romanize(92).should match 'XCII'
-  #end
+  end
 
   #it 'should encode triple digit numbers' do
   #  romanize(100).should match 'C'
