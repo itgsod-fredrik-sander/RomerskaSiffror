a804aafd9e00c67d6036d4d9e07a0089e4344ecf
Fredrik Sander <fredde.sander@gmail.com>
2014-05-22 09:26:16 +0200

    Duplicates the number argument

--- lib/romanize.rb
+++ lib/romanize.rb
@@ -13,11 +13,15 @@ class Integer
 end
 
 def romanize(number)
+  number = number.dup
+  
   raise ArgumentError, 'can not encode zero' if number.zero?
   raise ArgumentError, 'can not encode negative number' if number.negative?
   
   arabic_roman_numbers = {1 => "I", 5 => "V", 10 => "X", 50 => "L", 100 => "C", 500 => "D", 1000 => "M"}
   number = number.to_a
+
+
 
   return "string"
 end
