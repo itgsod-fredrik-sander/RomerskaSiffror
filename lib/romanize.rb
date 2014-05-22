3526bc1f519f41f330ec59765a02119325e24354
Fredrik Sander <fredde.sander@gmail.com>
2014-05-22 08:56:50 +0200

    Initialized arabic_roman_numbers hash

--- lib/romanize.rb
+++ lib/romanize.rb
@@ -12,7 +12,7 @@ def romanize(number)
   raise ArgumentError, 'can not encode zero' if number.zero?
   raise ArgumentError, 'can not encode negative number' if number.negative?
   
-  arabic_roman_numbers = {}
+  arabic_roman_numbers = {1 => "I", 5 => "V", 10 => "X", 50 => "L", 100 => "C", 500 => "D", 1000 => "M"}
 
   return "string"
 end
