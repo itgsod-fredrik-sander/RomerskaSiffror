d398eec85a9db7bb725310cbbcec2a07c1f23b05
Fredrik Sander <fredde.sander@gmail.com>
2014-05-22 09:52:36 +0200

    Iteration over arabic_roman_numbers keys

--- lib/romanize.rb
+++ lib/romanize.rb
@@ -16,8 +16,13 @@ def romanize(number)
   raise ArgumentError, 'can not encode zero' if number.zero?
   raise ArgumentError, 'can not encode negative number' if number.negative?
   
-  arabic_roman_numbers = {1 => "I", 4 => "IV", 5 => "V", 9 => "IX", 10 => "X", 40 => "XL", 50 => "L", 90 => "XC" 100 => "C", 400 => "CD", 500 => "D", 1000 => "M"}
+  arabic_roman_numbers = {1 => "I", 4 => "IV", 5 => "V", 9 => "IX", 10 => "X", 40 => "XL", 50 => "L", 90 => "XC", 100 => "C", 400 => "CD", 500 => "D", 1000 => "M"}
   number = number.to_a
+  roman_number = ""
   
+  arabic_roman_numbers.keys.sort.each do |key|
+    
+  end
+
   return "string"
 end
